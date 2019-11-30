429767e10892ae4f92fccb65b85ed1bf27c0367c
Louis Maxwell <louismaxwell@knights.ucf.edu>
2019-11-30 00:43:05 -0500

    Update Phrase.js

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -9,5 +9,25 @@
         this.phrase = phrase.toLowerCase();
      }
 
- 
+        /**
+        * Display phrase on game board
+        */
+     addPhraseToDisplay() {
+        //  Grabs the phrase header
+        let phraseHeader = document.getElementById("phrase").firstElementChild;
+        let listItem = document.createElement('li');
+
+        for (let i=0; i < this.phrase.length; i++) {
+            let grabCharacter = this.phrase[i];
+            if (grabCharacter !== " "){
+                listItem.innerHTML += `<li class="hide letter ${grabCharacter}">${grabCharacter}</li>`;
+                phraseHeader.append(listItem);
+            } else {
+                listItem.innerHTML += `<li class="space"> </li>`;
+                phraseHeader.append(listItem);
+            }
+        }
+     }
+
+     
  }
