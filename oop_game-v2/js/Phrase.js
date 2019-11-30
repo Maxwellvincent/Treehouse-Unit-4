dad9ed361696a8c4d2781e043e9e950c1d2aa8c5
Louis Maxwell <louismaxwell@knights.ucf.edu>
2019-11-29 19:19:31 -0500

    Update Phrase.js

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,13 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+//  Think of this as 1 object of a selected Phrase
+ class Phrase {
+     constructor(phrase) {
+            // this.phrase is creating a property called phrase for this object(class called phrase). we want to make it a string. 
+        this.phrase = phrase.toLowerCase();
+     }
+
+ 
+ }
