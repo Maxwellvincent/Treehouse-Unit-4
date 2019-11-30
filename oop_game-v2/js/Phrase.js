a446ee857120f82007475aacf080077224fec361
Louis Maxwell <louismaxwell@knights.ucf.edu>
2019-11-30 16:29:17 -0500

    Update Phrase.js

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -3,8 +3,9 @@
  * Phrase.js */
 
 //  Think of this as 1 object of a selected Phrase
+
  class Phrase {
-     constructor(phrase) {
+     constructor (phrase) {
             // this.phrase is creating a property called phrase for this object(class called phrase). we want to make it a string. 
         this.phrase = phrase.toLowerCase();
      }
@@ -12,22 +13,22 @@
         /**
         * Display phrase on game board
         */
+
      addPhraseToDisplay() {
-        //  Grabs the phrase header
-        let phraseHeader = document.getElementById("phrase").firstElementChild;
-        let listItem = document.createElement('li');
+            //  Grabs the phrase header
+            let phraseHeader = document.getElementById("phrase").firstElementChild;
+            let listItem = document.createElement('li');
 
-        for (let i=0; i < this.phrase.length; i++) {
-            let grabCharacter = this.phrase[i];
-            if (grabCharacter !== " "){
-                listItem.innerHTML += `<li class="hide letter ${grabCharacter}">${grabCharacter}</li>`;
-                phraseHeader.append(listItem);
-            } else {
-                listItem.innerHTML += `<li class="space"> </li>`;
-                phraseHeader.append(listItem);
-            }
+            for (let i=0; i < this.phrase.length; i++) {
+                let grabCharacter = this.phrase[i];
+                if (grabCharacter !== " "){
+                    listItem.innerHTML += `<li class="hide letter ${grabCharacter}">${grabCharacter}</li>`;
+                    phraseHeader.append(listItem);
+                } else {
+                    listItem.innerHTML += `<li class="space"> </li>`;
+                    phraseHeader.append(listItem);
+                }
+            }        
         }
-     }
 
-     
  }
