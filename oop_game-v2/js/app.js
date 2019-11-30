f6c71a897bd6e51ac506d49037acf22c7fa23c20
Louis Maxwell <louismaxwell@knights.ucf.edu>
2019-11-29 19:19:24 -0500

    Update app.js

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,3 +1,31 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+     // This WORKS!!
+//  Creating an instance of each class. Checked and they are working.
+//  const phrase = new Phrase();
+//  const game = new Game();
+
+const phrase = new Phrase(`Life is like a box of chocolates`);
+// phrase is the property of the phrase object. 
+console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+    // This WORKS!!
+const game = new Game();
+game.phrase.forEach((phrase, index) => {
+console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
+console.log(game);
+
+    // This WORKS!!
+// const logPhrase = (phrase) => {
+//     console.log(`Phrase - phrase: `, phrase.phrase);
+//     };
+//     const game = new Game();
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+
